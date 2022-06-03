cc157b55a543cda5866f625612d938ce5b162684
Cahanghir1 <jahanghiresh@code.edu.az>
2022-06-03 11:23:33 +0400

    Add project files.

---
+++ ClassLibrary1/Department.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    internal class Department
+    {
+        public string Name;
+        public int EmployeeLimit;
+        public int SalaryLimit;
+        private Employee[] _employees;
+
+        public Employee[] Employees
+        {
+            get { return _employees; }
+
+        }
+        public void AddEmployee(Employee employee1)
+        {
+            if (_employees.Length<EmployeeLimit)
+            {  
+                Array.Resize(ref _employees, _employees.Length + 1);
+                _employees[_employees.Length - 1] = employee1;
+            }
+            
+        }
+
+            
+    }
+}
---
+++ ClassLibrary1/Employee.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    internal class Employee
+    {
+        private string _name;
+        private string _surname;
+        private int _salary;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                bool check = false;
+                if (value!=null&&Char.IsUpper(value[0]))
+                {
+                    for (int i = 1; i < value.Length; i++)
+                        if (Char.IsLetter(value[i]))
+                        {
+                            check = true;
+                        }
+                        else
+                        {
+                            check=false;
+                            break;
+                        }
+                            
+
+                }
+                if (check)
+                {
+                    _name = value;
+                }
+
+            }
+        }
+        public string SurName
+        {
+            get { return _surname; }
+            set
+            { bool check  = false;
+                if (value!=null&&Char.IsUpper(value[0]))
+                {
+                    for(int i = 1; i < value.Length; i++)
+                        if (Char.IsLetter(value[i]))
+                        {
+                            check = true;
+                        }
+                   
+                }
+                if (check)
+                {
+                    _surname = value;
+                }
+
+            }
+
+        }
+        public int Salary
+        {
+            get { return _salary; }
+            set
+            {
+                if (value>249)
+                {
+                    _salary = value;
+
+                }
+            }
+        }
+
+        
+    }
+}
